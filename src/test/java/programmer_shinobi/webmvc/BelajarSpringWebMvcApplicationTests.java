4fb49a471b2953e38510312b25072bb95fcaa9e2
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-13 17:16:18 +0700

    [add] setup spring web MVC

---
+++ src/test/java/programmer_shinobi/webmvc/BelajarSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package programmer_shinobi.webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
