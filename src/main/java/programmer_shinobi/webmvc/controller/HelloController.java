97fc3eafac509e23e4324cf06581987ed1e1ddea
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-13 17:32:42 +0700

    [add] learn : request mapping

--- src/main/java/programmer_shinobi/webmvc/controller/HelloController.java
+++ src/main/java/programmer_shinobi/webmvc/controller/HelloController.java
@@ -1,7 +1,17 @@
 package programmer_shinobi.webmvc.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class HelloController {
+
+    @RequestMapping(path = "/hello")
+    public void helloWorld(HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello World");
+    }
+
 }
