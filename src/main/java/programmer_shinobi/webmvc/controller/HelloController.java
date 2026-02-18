2dad0b90a30fedcc4b9923cb93d36c321618d7a2
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-18 10:50:22 +0700

    [add] learn : request param

--- src/main/java/programmer_shinobi/webmvc/controller/HelloController.java
+++ src/main/java/programmer_shinobi/webmvc/controller/HelloController.java
@@ -1,12 +1,12 @@
 package programmer_shinobi.webmvc.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import programmer_shinobi.webmvc.service.HelloService;
 
 import java.io.IOException;
@@ -19,8 +19,8 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping(path = "/hello")
-    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
+    public void helloWorld(@RequestParam(name = "name", required = false) String name,
+                           HttpServletResponse response) throws IOException {
         String responseBody = helloService.hello(name);
         response.getWriter().println(responseBody);
     }
--- src/test/java/programmer_shinobi/webmvc/controller/DateControllerTest.java
+++ src/test/java/programmer_shinobi/webmvc/controller/DateControllerTest.java
@@ -1,48 +1,33 @@
 package programmer_shinobi.webmvc.controller;
 
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import programmer_shinobi.webmvc.service.HelloService;
-
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.MockMvcBuilder.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HelloControllerMockTest {
+class DateControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
-    private HelloService helloService;
-
-    @BeforeEach
-    void setUp() {
-
-        Mockito.when(helloService.hello(Mockito.anyString()))
-                .thenReturn("Hello Everyone");
-
-    }
-
     @Test
-    void helloName() throws Exception {
+    void date() throws Exception {
         mockMvc.perform(
-                get("/hello").queryParam("name", "Faqih")
+                get("/date").queryParam("date", "2028-10-10")
         ).andExpectAll(
                 status().isOk(),
-                content().string(Matchers.containsString("Hello Everyone"))
+                content().string(Matchers.containsString("Date : 20281010"))
         );
     }
 
