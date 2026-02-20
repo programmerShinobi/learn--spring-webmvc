5864731999e4485de1f1103d304ddb7b2c09bf1f
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-20 15:13:31 +0700

    [add] learn : MVC config

---
+++ src/main/java/programmer_shinobi/webmvc/MyWebConfig.java
@@ -0,0 +1,8 @@
+package programmer_shinobi.webmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+}
