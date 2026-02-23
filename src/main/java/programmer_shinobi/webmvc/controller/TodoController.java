0d0c85d93334c7f6de59dedd591bbd99eee00f70
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-23 13:38:32 +0700

    [add] learn : rest controller

---
+++ src/main/java/programmer_shinobi/webmvc/controller/TodoController.java
@@ -0,0 +1,29 @@
+package programmer_shinobi.webmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+    private List<String> todos = new ArrayList<>();
+
+    @PostMapping(path = "/todos", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> addTodo(@RequestParam(name = "todo")String todo) {
+        todos.add(todo);
+        return todos;
+    }
+
+    @GetMapping(value = "/todos", produces =  MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getTodo() {
+        return todos;
+    }
+
+}
